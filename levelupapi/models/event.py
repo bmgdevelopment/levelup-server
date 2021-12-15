c3bdfb000304b53492e50f5774264da540a5527f
Brittany Garrett <brittanymgarrett@gmail.com>
2021-12-14 20:31:32 -0600

    Fixtures made

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -8,3 +8,5 @@ class Event(models.Model):
     location = models.CharField(max_length=50)
     date = models.DateTimeField(max_length=10)
     time = models.TimeField(auto_now_add=True)
+    # attendees = models.ManyToManyField("levelupapi.Gamer", related_name="myevents")
+    
