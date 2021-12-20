a7f0dcfa3cbd5c2312c4336017062d23d361566a
Brittany Garrett <brittanymgarrett@gmail.com>
2021-12-19 23:38:11 -0600

    Updated

--- levelup.sql
+++ levelup.sql
@@ -3,3 +3,18 @@ SELECT * FROM authtoken_token;
 SELECT * FROM levelupapi_gamer;
 SELECT * FROM levelupapi_game;
 SELECT * FROM levelupapi_event;
+
+
+SELECT g.id,
+    g.gametype_id,
+    g.title,
+    g.maker,
+    g.gamer_id,
+    g.number_of_players,
+    g.skill_level
+FROM levelupapi_game g
+
+SELECT g.id,
+    g.gametype_id,
+    g.name
+FROM levelupapi_game g
--- levelup/urls.py
+++ levelup/urls.py
@@ -7,8 +7,3 @@ urlpatterns = [
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
-
-# Requests to http://localhost:8000/register will be routed to the register_user function
-# path('register', register_user)
-# Requests to http://localhost:8000/login will be routed to the login_user function
-# path('login', login_user)
