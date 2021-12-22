86748911a95e1e85be60585a46a0f5b61641db5c
Brittany Garrett <brittanymgarrett@gmail.com>
2021-12-21 20:53:05 -0600

    Updated

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,7 +4,7 @@ class Event(models.Model):
 
     title = models.CharField(max_length=55)
     game = models.ForeignKey("levelupapi.Game", on_delete=models.CASCADE)
-    gamer = models.ForeignKey("levelupapi.Gamer", on_delete=models.CASCADE, related_name="events")
+    gamer = models.ForeignKey("levelupapi.Gamer", on_delete=models.CASCADE)
     location = models.CharField(max_length=50)
     date = models.DateTimeField(max_length=10)
     time = models.TimeField(auto_now_add=True)
--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -3,6 +3,16 @@ from django.db import models
 
 class Game(models.Model):
     
+    class Skill(models.IntegerChoices):
+        BEGINNER = 1
+        EXPERIENCED = 2
+        PROFESSIONAL = 3
+        EXPERT = 4
+    
+    title = models.CharField(max_length=50)
+    maker = models.CharField(max_length=50)
+    gamer = models.ForeignKey("levelupapi.Gamer", on_delete=models.CASCADE)
     gametype = models.ForeignKey("levelupapi.GameType", on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
+    number_of_players = models.IntegerField()
+    skill_level = models.IntegerField(choices=Skill.choices)
     
